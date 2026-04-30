5b65f36daf617358e9003917383fe755e47c1840
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-04-30 14:24:48 +0000

    create fun

---
+++ R/extract_JVS.R
@@ -0,0 +1,10 @@
+extract_QR <- function(
+        file,
+        x,
+        matrix_output_file,
+        sep = ";",
+        dec = ",",
+        thresholds = getOption("jdc_thresholds")
+) {
+    return(NULL)
+}
