175527e3b5d376b9aaa85665019aa4b7305c8190
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-06-12 09:50:18 +0000

    document JVS_matrix

--- R/JVS_classes.R
+++ R/JVS_classes.R
@@ -1,7 +1,52 @@
 #' @title JVS matrix object
+#'
+#' @description
+#' \code{JVS_matrix()} are creating a quality report based on the Eurostat JVS
+#' Plug-In.
+#'
+#' @param x a \code{data.frame} containing the output variables' values
+#' (test p-values, test statistics, etc.) and modalities (Yes/No).
+#'
+#' @details A\code{\link{QR_matrix}} object is a data.frame with 30 items:
+#'
+#' * Series
+#' * Method
+#' * Period
+#' * Nobs
+#' * Start
+#' * End
+#' * Adjustment
+#' * Presence of Seasonality in the Raw Series
+#' * Presence of TD effects
+#' * Log-Transformation
+#' * ARIMA Model
+#' * LeapYear
+#' * MovingHoliday
+#' * NbTD
+#' * Noutliers
+#' * Outlier1
+#' * Outlier2
+#' * Outlier3
+#' * Residual Seasonality in SA Series (F-test)
+#' * Residual TD Effect
+#' * Q-Stat (for X13)
+#' * Final Henderson Filter
+#' * Stage 2 Henderson Filter
+#' * Seasonal Filter
+#' * Quality
+#' * Autocorrelation of order 1 of the SA series
+#' * Ljung-Box Test (P-value)
+#' * Autocorrelation negative and significant
+#' * Irregular Standard-Deviation
+#' * Max-Adj
+#'
+#' @returns
+#' \code{JVS_matrix()} creates and returns a \code{\link{JVS_matrix}} object.
+#'
+#' @encoding UTF-8
 #' @name JVS_matrix
 #' @export
-JVS_matrix <- function(x = list(), ...) {
+JVS_matrix <- function(x = list()) {
     UseMethod("JVS_matrix", x)
 }
 
