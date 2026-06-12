5264eaa25f94248e632869b13825adf72fefd316
Eulalie Delaune <eulalie.delaune@eleve.ensai.fr>
2026-06-12 11:51:33 +0200

    add param ... in JVS_matrix

--- R/JVS_classes.R
+++ R/JVS_classes.R
@@ -54,7 +54,7 @@ JVS_matrix <- function(x = list()) {
 #' @exportS3Method JVS_matrix data.frame
 #' @method JVS_matrix data.frame
 #' @export
-JVS_matrix.data.frame <- function(x) {
+JVS_matrix.data.frame <- function(x, ...) {
     class(x) <- c("JVS_matrix", "data.frame")
     return(x)
 }
@@ -63,7 +63,7 @@ JVS_matrix.data.frame <- function(x) {
 #' @exportS3Method JVS_matrix JVS_matrix
 #' @method JVS_matrix JVS_matrix
 #' @export
-JVS_matrix.JVS_matrix <- function(x) {
+JVS_matrix.JVS_matrix <- function(x, ...) {
     return(x)
 }
 
@@ -71,6 +71,6 @@ JVS_matrix.JVS_matrix <- function(x) {
 #' @exportS3Method JVS_matrix default
 #' @method JVS_matrix default
 #' @export
-JVS_matrix.default <- function(x) {
+JVS_matrix.default <- function(x, ...) {
     stop("A JVS_matrix or data.frame object is required!", call. = FALSE)
 }
